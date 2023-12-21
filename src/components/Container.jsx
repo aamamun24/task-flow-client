e5ef21f4e4b19f66af01a0ebd876f5e17c0992ea
aamamun24 <bdmamun100@gmail.com>
2023-12-21 16:53:40 +0600

    footer added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
     return (
-        <div className='max-w-7xl mx-auto px-4 md:px-8 lg:px-12'>
+        <div className='max-w-7xl w-full mx-auto px-4 md:px-8 lg:px-12'>
             {children}
         </div>
     );
