3b682b95a7f3dd4a6a04183308602b5b599f209b
aamamun24 <bdmamun100@gmail.com>
2023-12-21 16:35:22 +0600

    navbar created

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const navLinks = [
+    {
+        id: 1,
+        to: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        to: 'about',
+        name: 'About'
+    },
+    {
+        id: 3,
+        to: 'dashboard',
+        name: 'Dashboard'
+    },
+]
